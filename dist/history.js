bf3f193d89ca1b9262962005dd2efb1bab8e1fc9
Richard Harrington <rwharrington87@gmail.com>
2016-02-18 13:40:16 -0500

    fix userRouterHistory call

--- dist/history.js
+++ dist/history.js
@@ -8,7 +8,7 @@ exports.createHistory = createHistory;
 var _reactRouter = require('react-router');
 
 function createHistory(windowExists) {
-  return windowExists ? (0, _reactRouter.useRouterHistory)(_reactRouter.browserHistory) : (0, _reactRouter.useRouterHistory)(_reactRouter.memoryHistory);
+  return windowExists ? (0, _reactRouter.useRouterHistory)(_reactRouter.browserHistory)() : (0, _reactRouter.useRouterHistory)(_reactRouter.memoryHistory)();
 }
 
 exports.default = createHistory(typeof window !== 'undefined');
--- src/history.js
+++ src/history.js
@@ -1,7 +1,7 @@
 import { useRouterHistory, memoryHistory, browserHistory } from 'react-router';
 
 export function createHistory(windowExists) {
-  return windowExists ? useRouterHistory(browserHistory) : useRouterHistory(memoryHistory);
+  return windowExists ? useRouterHistory(browserHistory)() : useRouterHistory(memoryHistory)();
 }
 
 export default createHistory(typeof window !== 'undefined');
